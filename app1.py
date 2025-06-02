29f1dddab9a11c9e3be65f6ee56d373871252f16
7-cut <119734514+7-cut@users.noreply.github.com>
2025-06-02 18:44:52 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -24,7 +24,7 @@ app.config['UPLOAD_FOLDER'] = 'data/cache'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '@ceg35AD1'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'projectlaundry'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
